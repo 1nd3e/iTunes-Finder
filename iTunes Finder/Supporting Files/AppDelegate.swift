6f962d9335b004593dfe7da26a58d782e3e1963b
Vladislav Len <vlad@1nd3e.ru>
2020-12-25 17:54:38 +0300

    Deleted unnecessary files and organized a project structure

---
+++ iTunes Finder/Supporting Files/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  iTunes Finder
+//
+//  Created by Vladislav Len on 25.12.2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+    
+    var window: UIWindow?
+    
+}
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
